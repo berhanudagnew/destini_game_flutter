b40ee1b099ab344cfcdd53bcda66a614ace54b9b
berhanudagnew <berhanudagnew.github@gmail.com>
2022-09-29 14:49:00 +0300

    fix  TODO 15

--- lib/story.dart
+++ lib/story.dart
@@ -6,5 +6,6 @@ class Story {
   String choice1;
   String choice2;
 //TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
-  Story({this.storyTitle, this.choice1, this.choice2});
+  Story(
+      {required this.storyTitle, required this.choice1, required this.choice2});
 }
