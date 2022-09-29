0c844586f899c2d53f4d0a438a05663c6cbb0ede
berhanudagnew <berhanudagnew.github@gmail.com>
2022-09-29 08:30:07 +0300

    fix  TODO 8

--- lib/story.dart
+++ lib/story.dart
@@ -2,9 +2,9 @@
 class Story {
 //TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
 
-  String storyTytle;
+  String storyTitle;
   String choice1;
   String choice2;
 //TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
-  Story(this.storyTytle, this.choice1, this.choice2);
+  Story({this.storyTitle, this.choice1, this.choice2});
 }
