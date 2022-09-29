299710f3f6c68732c70e8e404c07af02e2689cdf
berhanudagnew <berhanudagnew.github@gmail.com>
2022-09-29 16:17:30 +0300

    fix  TODO 25 update next story

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -40,20 +40,20 @@ class StoryBrain {
   ];
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
-  int storyNumber = 0;
+  int _storyNumber = 0;
 //TODO: Step 8 - Create a method called getStory() that returns the first storyTitle from _storyData.
   String getStory() {
-    return _storyData[storyNumber].storyTitle;
+    return _storyData[_storyNumber].storyTitle;
   }
 
 //TODO: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
   String getChoice1() {
-    return _storyData[storyNumber].choice1;
+    return _storyData[_storyNumber].choice1;
   }
 
 //TODO: Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
   String getChoice2() {
-    return _storyData[storyNumber].choice2;
+    return _storyData[_storyNumber].choice2;
   }
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
 
@@ -65,20 +65,20 @@ class StoryBrain {
 
 //TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
   void nextStory(int choiceNumber) {
-    if (choiceNumber == 1 && storyNumber == 0) {
-      storyNumber = 2;
-    } else if (choiceNumber == 2 && storyNumber == 0) {
-      storyNumber = 1;
-    } else if (choiceNumber == 1 && storyNumber == 2) {
-      storyNumber = 5;
-    } else if (choiceNumber == 2 && storyNumber == 2) {
-      storyNumber = 4;
-    } else if (choiceNumber == 1 && storyNumber == 1) {
-      storyNumber = 2;
-    } else if (choiceNumber == 2 && storyNumber == 1) {
-      storyNumber = 3;
-    } else if (storyNumber == 3 || storyNumber == 4 || storyNumber == 5) {
-      storyNumber = 0;
+    if (choiceNumber == 1 && _storyNumber == 0) {
+      _storyNumber = 2;
+    } else if (choiceNumber == 2 && _storyNumber == 0) {
+      _storyNumber = 1;
+    } else if (choiceNumber == 1 && _storyNumber == 2) {
+      _storyNumber = 5;
+    } else if (choiceNumber == 2 && _storyNumber == 2) {
+      _storyNumber = 4;
+    } else if (choiceNumber == 1 && _storyNumber == 1) {
+      _storyNumber = 2;
+    } else if (choiceNumber == 2 && _storyNumber == 1) {
+      _storyNumber = 3;
+    } else if (_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5) {
+      _storyNumber = 0;
     }
   }
 //TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
